680109cf01483fc1dc8df50be58053fc8db8a86c
Sergey Kutenniy <kutenniysergey@gmail.com>
2023-01-25 21:46:47 +0200

    up to bem

--- js/console.js
+++ js/console.js
@@ -7,3 +7,13 @@
     e.currentTarget.reset();
   });
 })();
+
+(() => {
+  document.querySelector('.js-contact-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
+
+    e.currentTarget.reset();
+  });
+})();
